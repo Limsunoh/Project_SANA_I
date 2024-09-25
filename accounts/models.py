68aacffcb40a19771735c97e60b7a0cb6230c80d
임선오 <vhtmxj154@naver.com>
2024-09-25 20:27:20 +0900

    update: signup, login, logout
    회원가입시 메일 인증을 통해 메일인증이 되어야 로그인이 되도록 구현,
    로그인 및 로그아웃 기능 구현

--- accounts/models.py
+++ accounts/models.py
@@ -9,8 +9,8 @@ class User(AbstractUser):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=255)
     birth = models.DateField()
-    email = models.EmailField(max_length=30, unique=True, null=False, blank=False)
+    email = models.EmailField(max_length=30, unique=False, null=False, blank=False)
     created_at = models.TimeField(auto_now_add=True)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="images/")
     introduce = models.TextField(max_length=255)
     
--- sbmarket/settings.py
+++ sbmarket/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = "sbmarket.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates',],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -128,7 +128,14 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
