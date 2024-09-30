03a488b512f92d4cc486b79e304e4418f97423c2
임선오 <vhtmxj154@naver.com>
2024-09-30 11:09:12 +0900

    fix:settings. ALLOWED_HOSTS

--- sbmarket/settings.py
+++ sbmarket/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = SANAI_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",
+                 "54.180.134.131",
+                 "127.0.0.1",]
 
 
 # Application definition
