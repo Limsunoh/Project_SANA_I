abba38c94d01eb10477a219c36a8fd9d44028fe6
임선오 <vhtmxj154@naver.com>
2024-10-02 20:52:51 +0900

    add: static file

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('signup/', views.UserCreateView.as_view(), name='user-signup'),
+    path('user_signup/', views.signup_view, name='signup'),
+    path('api/email-verify/', views.EmailVerifyView.as_view(), name='email-verify'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/activate/<pk>/<token>/', views.activate_user, name='activate_user'),
