669ef4f8b2e17a2c651b3a9e56a97acf7e11c715
임선오 <vhtmxj154@naver.com>
2024-10-06 18:36:57 +0900

    fix: 테스트 코드 삭제 및 로고 클릭시 home 주소 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,22 +6,22 @@ from rest_framework_simplejwt.views import (
     TokenBlacklistView,
 )
 
-app_name = 'accounts'
+app_name = "accounts"
 urlpatterns = [
     # API 앤드포인트
-    path('signup/', views.UserCreateView.as_view(), name='user-signup'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/activate/<pk>/<token>/', views.activate_user, name='activate_user'),
+    path("signup/", views.UserCreateView.as_view(), name="user-signup"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("users/activate/<pk>/<token>/", views.activate_user, name="activate_user"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("logout/", TokenBlacklistView.as_view(), name='logout'),
-    path('profile/<str:username>/', views.UserProfileView.as_view()),
-    path('profile/<str:username>/password/', views.ChangePasswordView.as_view(),),
-    path('follow/<str:username>/', views.FollowView.as_view(), name='follow_view'),
-    
+    path("logout/", TokenBlacklistView.as_view(), name="logout"),
+    path("profile/<str:username>/", views.UserProfileView.as_view()),
+    path(
+        "profile/<str:username>/password/",
+        views.ChangePasswordView.as_view(),
+    ),
+    path("follow/<str:username>/", views.FollowView.as_view(), name="follow_view"),
     
     # 프론트(화면구성) 주소
-    path('signup-page/', views.SignupPageView.as_view(), name='signup-page'),
+    path("signup-page/", views.SignupPageView.as_view(), name="signup-page"),
     path("login-page/", views.LoginPageView.as_view(), name="token_obtain_pair"),
-    
-    
 ]
