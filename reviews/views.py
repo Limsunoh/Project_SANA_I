8f3267ece0486f31bd02eae8d3b9d3beff93e2cc
unknown <tkdgus1569@gmail.com>
2024-10-07 04:18:52 +0900

    feat: 리뷰 생성

--- reviews/views.py
+++ reviews/views.py
@@ -6,12 +6,12 @@ from .serializers import ReviewSerializer
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         # 리뷰가 이미 존재하는지 체크
-        product = serializer.validated_data['product']
-        if Review.objects.filter(author=self.request.user, product=product).exists():
-            raise serializers.ValidationError("해당 상품에 대한 리뷰는 이미 작성되었습니다.")
+        # product = serializer.validated_data['product']
+        # if Review.objects.filter(author=self.request.user, product=product).exists():
+        #     raise serializers.ValidationError("해당 상품에 대한 리뷰는 이미 작성되었습니다.")
         # 리뷰 생성
         serializer.save(author=self.request.user)
