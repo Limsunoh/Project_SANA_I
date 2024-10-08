4fd233818ab1d62d2f6d1a60fdae034f4c7f6fd5
kwang1215 <xman1215@naver.com>
2024-10-08 15:49:37 +0900

    fix: 찜하기 기능 코드수정

--- products/urls.py
+++ products/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # API 앤드포인트
     path("", views.ProductListAPIView.as_view(), name="product_list"),
     path("<int:pk>/", views.ProductDetailAPIView.as_view(), name="product_detail"),
-    path('<int:pk>/like/', views.LikeAPIView.as_view(), name='like'),  # 찜하기 및 찜한 목록 조회
+    path('<int:pk>/like/', views.LikeAPIView.as_view(), name='like'),  # 개별 제품 찜 상태 확인 및 찜하기 기능
     path("<int:pk>/comment/", views.CommentListCreateView.as_view(), name='comment'),
     path('<int:product_id>/chatrooms/', views.ChatRoomCreateAPIView.as_view(), name='create-chatroom'),  # 채팅방 생성
     path('<int:pk>/chatrooms/<int:room_id>/messages/', views.ChatMessageCreateAPIView.as_view(), name='create-message'),
