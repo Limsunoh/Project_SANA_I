b21e9352d0ef3bea0bb57164cca38822208e2da6
unknown <tkdgus1569@gmail.com>
2024-10-10 20:04:24 +0900

    fix: 앱 중첩, 오타

--- sbmarket/settings.py
+++ sbmarket/settings.py
@@ -40,12 +40,6 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-
-    # Local
-    "accounts",
-    "products",
-    "manager",  
-
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
