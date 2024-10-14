35f535909791d556b369425979383240761df914
unknown <tkdgus1569@gmail.com>
2024-10-14 11:03:04 +0900

    fix: product detail page에서 리뷰 내용 보기 가능해짐

--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,19 @@
-from rest_framework import viewsets, serializers
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, permissions
 from .models import Review
 from .serializers import ReviewSerializer
 
-class ReviewViewSet(viewsets.ModelViewSet):
+# 리뷰 목록 조회 및 생성
+class ReviewListCreateView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        # 리뷰가 이미 존재하는지 체크
-        # product = serializer.validated_data['product']
-        # if Review.objects.filter(author=self.request.user, product=product).exists():
-        #     raise serializers.ValidationError("해당 상품에 대한 리뷰는 이미 작성되었습니다.")
-        # 리뷰 생성
+        # 점수 계산 후 저장
         serializer.save(author=self.request.user)
+
+# 리뷰 조회 및 삭제
+class ReviewDetailView(generics.RetrieveDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
