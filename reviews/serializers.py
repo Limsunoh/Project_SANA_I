f6b415ed74730048797298d0fcad35897baaa6df
kwang1215 <xman1215@naver.com>
2024-10-14 20:28:43 +0900

    chat 구매완료 기능

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,20 +1,35 @@
 from rest_framework import serializers
 from .models import Review
+from products.models import Product
 
 class ReviewSerializer(serializers.ModelSerializer):
     checklist = serializers.ListField(
         child=serializers.CharField(max_length=100)
     )
-    
+
     class Meta:
         model = Review
-        fields = ['id', 'author', 'products', 'checklist', 'additional_comments', 'created_at', 'score']
+        fields = ['id', 'author', 'checklist', 'additional_comments', 'created_at', 'score']
         read_only_fields = ['author', 'created_at', 'score']
 
     def create(self, validated_data):
-        # 리뷰를 먼저 생성한 후, score를 계산합니다.
-        checklist = validated_data.get('checklist', [])
+
+        # URL에 제품 pk참조
+        request = self.context.get('request')
+        validated_data['author'] = request.user
+        product_pk = self.context['view'].kwargs.get('product_id')
+
+        try:
+            product = Product.objects.get(pk=product_pk)
+        except Product.DoesNotExist:
+            raise serializers.ValidationError(f"{product_pk} 게시글이 없습니다.")
+
+        validated_data['products'] = product
+
+        # 리뷰 생성 & 점수 계산
+        # checklist = validated_data.get('checklist', [])
         review = Review.objects.create(**validated_data)
-        review.score = review.total_score()  # 총점 계산
+        review.score = review.total_score()
         review.save()
+
         return review
--- reviews/urls.py
+++ reviews/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ReviewListCreateView.as_view(), name='review-list-create'),
+    # 유저가 작성한 리뷰 목록 조회 (유저 프로필 페이지)
+    path('user/<int:user_id>/', views.UserReviewListView.as_view(), name='user-review-list'),
+
+    # 제품 상세 페이지에서 리뷰 작성
+    path('products/<int:product_id>/', views.ReviewCreateView.as_view(), name='product-review-create'),
+
+    # 리뷰 상세 조회 및 삭제
     path('<int:pk>/', views.ReviewDetailView.as_view(), name='review-detail'),
-    # path('reviews/<int:pk>/destroy/', views.ReviewDetailView.as_view(), name='review-destroy'),
 ]
