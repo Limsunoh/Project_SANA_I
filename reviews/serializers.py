62543c08ef5f2a7a9f1bb5cd9e6a86bcc009da5e
임선오 <vhtmxj154@naver.com>
2024-10-14 22:57:17 +0900

    feat: chat_alarm 구현
    안읽은 채팅 5초마다 페이지 리로드(롱폴링방식)를 통해 안읽은 채팅이
    있을경우 몇개의 채팅을 안읽었는지 내 채팅방 옆에 표시되게끔 구현

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,19 +1,26 @@
 from rest_framework import serializers
 from .models import Review
 
+
 class ReviewSerializer(serializers.ModelSerializer):
-    checklist = serializers.ListField(
-        child=serializers.CharField(max_length=100)
-    )
-    
+    checklist = serializers.ListField(child=serializers.CharField(max_length=100))
+
     class Meta:
         model = Review
-        fields = ['id', 'author', 'products', 'checklist', 'additional_comments', 'created_at', 'score']
-        read_only_fields = ['author', 'created_at', 'score']
+        fields = [
+            "id",
+            "author",
+            "products",
+            "checklist",
+            "additional_comments",
+            "created_at",
+            "score",
+        ]
+        read_only_fields = ["author", "created_at", "score"]
 
     def create(self, validated_data):
         # 리뷰를 먼저 생성한 후, score를 계산합니다.
-        checklist = validated_data.get('checklist', [])
+        checklist = validated_data.get("checklist", [])
         review = Review.objects.create(**validated_data)
         review.score = review.total_score()  # 총점 계산
         review.save()
